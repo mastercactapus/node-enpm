f5b1e87ffc46a028b14a735c015a4840290a62b9
Nathan Caza <mastercactapus@gmail.com>
2014-01-08 22:38:07 -0600

    make installer handle package.json

--- install.js
+++ install.js
@@ -4,6 +4,8 @@ var request = require("request");
 var semver = require("semver");
 var mkdirp = require("mkdirp");
 var _ = require("lodash");
+var path = require("path");
+var fs = require("fs");
 
 var versions = require("./lib/versions");
 var packageData = require("./lib/package-data");
@@ -15,17 +17,32 @@ var pkgSplit = pkg.split("@");
 var pkgName = pkgSplit[0];
 var pkgVers = pkgSplit[1] || "";
 
-if (!pkgName) {
-	console.error("Specify a package as first parameter: mypackage[@~0.0.1]");
-	process.exit(1);
-}
-
 var depMap = new DependencyMapper();
 
-depMap.addDependency(pkgName, pkgVers)
+var deps = {};
+if (!pkgName) {
+	if (fs.existsSync("package.json")) {
+		deps = JSON.parse(fs.readFileSync("package.json")).dependencies||{};
+	} else {
+		console.error("You must specify a package, or run this in a directory with package.json");
+		process.exit(1);
+	}
+} else {
+	deps[pkgName] = pkgVers;
+}
+
+
+depMap.addDependencies(deps)
 .then(function(){
 	console.log("Installing packages...");
-	return PackageInstaller.installAll("./poc", depMap.dependencies);
+	return PackageInstaller.installAll("node_modules/.packages", depMap.dependencies);
+})
+.then(function(){
+	_.each(deps, function(semver, name){
+		var latest = versions.latest(depMap.dependencies[name], semver);
+		var dest = path.resolve("node_modules/.packages", name, latest);
+		fs.symlinkSync(dest, path.join("node_modules",name), "dir");
+	});
 })
 .then(function(){
 	console.log("Packages installed.");
